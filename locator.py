a0b5bbd104811d7963dae66199360fca6c35517f
Nicholas Catanchin <nicholas@catanchin.id.au>
2022-07-03 22:55:49 +1000

    Update indentation to spaces

--- locator.py
+++ locator.py
@@ -4,33 +4,33 @@ import logging
 import sys
 
 class Locator:
-	def __init__(self, logger : logging.Logger):
-		self.logger = logger
-		self.cmd = 'lolcate' if self.has_lolcate() else None
-		# self.limit = 5
-		self.opt = ''
+    def __init__(self, logger : logging.Logger):
+        self.logger = logger
+        self.cmd = 'lolcate' if self.has_lolcate() else None
+        # self.limit = 5
+        self.opt = ''
 
-	# def set_limit(self, limit : int) -> None:
-	# 	self.logger.debug('set limit to '+str(limit))
-	# 	self.limit = limit
+    # def set_limit(self, limit : int) -> None:
+    # 	self.logger.debug('set limit to '+str(limit))
+    # 	self.limit = limit
 
-	# def set_locate_opt(self, opt):
-	# 	print('set locate opt to '+opt)
-	# 	self.opt = opt
+    # def set_locate_opt(self, opt):
+    # 	print('set locate opt to '+opt)
+    # 	self.opt = opt
 
-	def has_lolcate(self) -> bool:
-		try:
-			subprocess.check_call(['which', 'lolcate'])
-			return True
-		except:
-			return False
+    def has_lolcate(self) -> bool:
+        try:
+            subprocess.check_call(['which', 'lolcate'])
+            return True
+        except:
+            return False
 
-	def run(self, pattern) -> list[str]:
-		if self.cmd == None:
-			raise RuntimeError('command lolcate not found or options config error')
-		else:
+    def run(self, pattern) -> list[str]:
+        if self.cmd == None:
+            raise RuntimeError('command lolcate not found or options config error')
+        else:
             cmd = [self.cmd, '-l', str(self.limit)]
-			args = pattern.split(' ')
+            args = pattern.split(' ')
 
             # if args[0] == 'r':
             #     cmd.extend(args[1:])
@@ -38,14 +38,14 @@ class Locator:
             #     cmd.append(self.opt)
             #     cmd.extend(args)
 
-			cmd.extend(args)
+            cmd.extend(args)
 
-			self.logger.debug(f"executing {cmd}")
+            self.logger.debug(f"executing {cmd}")
             print('----->'+str(cmd))
 
             output = subprocess.check_output(cmd, encoding='utf-8')
-			split = output.splitlines()
-			# self.logger.debug(f"{split}")
-			count = len(split)
-			self.logger.debug(f"found {count} matches")
-			return split
+            split = output.splitlines()
+            # self.logger.debug(f"{split}")
+            count = len(split)
+            self.logger.debug(f"found {count} matches")
+            return split
--- main.py
+++ main.py
@@ -22,96 +22,96 @@ logger = logging.getLogger(__name__)
 locator = Locator(logger)
 
 class Config:
-	limit = 5
+    limit = 5
 
 class SearchFileExtension(Extension):
-	def __init__(self):
-		super(SearchFileExtension, self).__init__()
-		self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-		self.subscribe(PreferencesEvent, PreferencesEventListener())
-		self.subscribe(PreferencesUpdateEvent, PreferencesUpdateEventListener())
-		self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+    def __init__(self):
+        super(SearchFileExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(PreferencesEvent, PreferencesEventListener())
+        self.subscribe(PreferencesUpdateEvent, PreferencesUpdateEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class PreferencesUpdateEventListener(EventListener):
-	def on_event(self, event, extension):
-		if event.id == 'limit':
-			Config.limit = int(event.new_value)
+    def on_event(self, event, extension):
+        if event.id == 'limit':
+            Config.limit = int(event.new_value)
 
 
 class PreferencesEventListener(EventListener):
-	def on_event(self, event, extension):
-		Config.limit = int(event.preferences['limit'])
+    def on_event(self, event, extension):
+        Config.limit = int(event.preferences['limit'])
 
 
 class ItemEnterEventListener(EventListener):
-	def on_event(self, event, extension):
-		results = event.get_data()
-		items = []
-		for file in results:
-			items.append(ExtensionSmallResultItem(icon='images/copy.png',
-				name = file,
-				on_enter = CopyToClipboardAction(file)))
-		return RenderResultListAction(items)
+    def on_event(self, event, extension):
+        results = event.get_data()
+        items = []
+        for file in results:
+            items.append(ExtensionSmallResultItem(icon='images/copy.png',
+                name = file,
+                on_enter = CopyToClipboardAction(file)))
+        return RenderResultListAction(items)
 
 
 class KeywordQueryEventListener(EventListener):
-	def __lolcateNotFound(self) -> list[ExtensionResultItem]:
-		items = []
-		items.append(ExtensionResultItem(icon='images/error.png',
-									name="no lolcate executable",
-									description="missing or not in PATH",
-									on_enter=OpenUrlAction("https://github.com/ngirard/lolcate-rs")
-									))
-		return items
-	
-	def __noArgs(self):
-		items = []
-		items.append(ExtensionResultItem(icon='images/info.png',
-									name="update default database",
-									description="re-index default database, may take time",
-									on_enter=RunScriptAction("lolcate --update")
-									))
-		return items
-				
-	def on_event(self, event, extension) -> RenderResultListAction:
-		arg = event.get_argument()
-		items = []
+    def __lolcateNotFound(self) -> list[ExtensionResultItem]:
+        items = []
+        items.append(ExtensionResultItem(icon='images/error.png',
+                                    name="no lolcate executable",
+                                    description="missing or not in PATH",
+                                    on_enter=OpenUrlAction("https://github.com/ngirard/lolcate-rs")
+                                    ))
+        return items
+    
+    def __noArgs(self):
+        items = []
+        items.append(ExtensionResultItem(icon='images/info.png',
+                                    name="update default database",
+                                    description="re-index default database, may take time",
+                                    on_enter=RunScriptAction("lolcate --update")
+                                    ))
+        return items
+                
+    def on_event(self, event, extension) -> RenderResultListAction:
+        arg = event.get_argument()
+        items = []
 
-		if not locator.has_lolcate():
-			logger.info("lolcate missing!")
-			return RenderResultListAction(self.__lolcateNotFound())	
-		if arg is None:
-			logger.info("no args provided")
-			items = self.__noArgs()
-			pass
-		else:
-			try:
-				results = locator.run(arg)
-				totalMatches = len(results)
-				logger.debug(f"assembling {min(Config.limit, totalMatches)} results")
-				for match in results[:Config.limit]:
-					matchName = match.decode("utf-8")
-					logger.debug(f"match: {matchName}")
-					items.append(ExtensionResultItem(icon='images/ok.png',
-						name = matchName.split("/")[-1], #only file/dir name
-						description = matchName, # full path
-						on_enter = OpenAction(match),
-						on_alt_enter = CopyToClipboardAction(match)))
-				if totalMatches > Config.limit:
-					extra = totalMatches - Config.limit
-					logger.info(f"showing {totalMatches}, another {extra} matches hidden")
-					items.append(ExtensionSmallResultItem(
-						icon='images/info.png',
-						name = f"...and {extra} more matches",
-						highlightable = False)) # TODO: add option to load more results
-			except Exception as e:
-				error_info = str(e)
-				items = [ExtensionSmallResultItem(icon='images/error.png',
-												name = error_info,
-												on_enter = CopyToClipboardAction(error_info))]
-		
-		return RenderResultListAction(items)
+        if not locator.has_lolcate():
+            logger.info("lolcate missing!")
+            return RenderResultListAction(self.__lolcateNotFound())	
+        if arg is None:
+            logger.info("no args provided")
+            items = self.__noArgs()
+            pass
+        else:
+            try:
+                results = locator.run(arg)
+                totalMatches = len(results)
+                logger.debug(f"assembling {min(Config.limit, totalMatches)} results")
+                for match in results[:Config.limit]:
+                    matchName = match.decode("utf-8")
+                    logger.debug(f"match: {matchName}")
+                    items.append(ExtensionResultItem(icon='images/ok.png',
+                        name = matchName.split("/")[-1], #only file/dir name
+                        description = matchName, # full path
+                        on_enter = OpenAction(match),
+                        on_alt_enter = CopyToClipboardAction(match)))
+                if totalMatches > Config.limit:
+                    extra = totalMatches - Config.limit
+                    logger.info(f"showing {totalMatches}, another {extra} matches hidden")
+                    items.append(ExtensionSmallResultItem(
+                        icon='images/info.png',
+                        name = f"...and {extra} more matches",
+                        highlightable = False)) # TODO: add option to load more results
+            except Exception as e:
+                error_info = str(e)
+                items = [ExtensionSmallResultItem(icon='images/error.png',
+                                                name = error_info,
+                                                on_enter = CopyToClipboardAction(error_info))]
+        
+        return RenderResultListAction(items)
 
 """     def __help(self):
         all_opt = opts.get_all()
@@ -160,4 +160,4 @@ class KeywordQueryEventListener(EventListener):
 
 
 if __name__ == '__main__':
-	SearchFileExtension().run()
+    SearchFileExtension().run()
